adb05cf200a5fc101ef12a9890e944df31ce5c01
dilshad <dilshadbasith01@gmail.com>
2023-07-11 16:13:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [DarkMode, setDarkMode] = useState();
+
+  const ThemeChange = () => {
+    setDarkMode(!DarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${DarkMode ? 'dark' : 'light'}`}>
+      <button onClick={ThemeChange} className="btn">
+        Switch Theme
+      </button>
     </div>
   );
 }
 
 export default App;
+
